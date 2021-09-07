cbad40ab265f673878e1b871b13ff65b52f8d235
Matthew Buckton <matthew@buckton.org>
2021-09-07 14:41:38 +1000

    make it abstract

--- src/main/java/io/mapsmessaging/storage/Storage.java
+++ src/main/java/io/mapsmessaging/storage/Storage.java
@@ -1,80 +1,12 @@
 package io.mapsmessaging.storage;
 
-import java.util.Collection;
 import java.util.Map;
-import java.util.Set;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-public class Storage<T extends Storable> implements Map<Long, T> {
+public abstract class Storage<T extends Storable> implements Map<Long, T> {
 
-  private final Factory<T> factory;
+  protected final Factory<T> factory;
 
   public Storage(Factory<T> factory){
     this.factory = factory;
-  }
-
-  @Override
-  public int size() {
-    return 0;
-  }
-
-  @Override
-  public boolean isEmpty() {
-    return false;
-  }
-
-  @Override
-  public boolean containsKey(Object key) {
-    return false;
-  }
-
-  @Override
-  public boolean containsValue(Object value) {
-    return false;
-  }
-
-  @Override
-  public T get(Object key) {
-    return null;
-  }
-
-  @Nullable
-  @Override
-  public T put(Long key, T value) {
-    return null;
-  }
-
-  @Override
-  public T remove(Object key) {
-    return null;
-  }
-
-  @Override
-  public void putAll(@NotNull Map<? extends Long, ? extends T> m) {
-
-  }
-
-  @Override
-  public void clear() {
-
-  }
-
-  @NotNull
-  @Override
-  public Set<Long> keySet() {
-    return null;
-  }
-
-  @NotNull
-  @Override
-  public Collection<T> values() {
-    return null;
-  }
-
-  @NotNull
-  @Override
-  public Set<Entry<Long, T>> entrySet() {
-    return null;
   }
 }
