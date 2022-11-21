844c67eb15a134d29413e2993a242945ee1e1cff
Matthew Buckton <matthew@buckton.org>
2022-11-21 13:04:11 +1100

    Initial work for S3 data migration for archival of stale data
    Signed-off-by: Matthew Buckton <matthew@buckton.org>

---
+++ src/main/java/io/mapsmessaging/storage/impl/file/s3/S3Record.java
@@ -0,0 +1,81 @@
+/*
+ *   Copyright [2020 - 2022]   [Matthew Buckton]
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ */
+
+package io.mapsmessaging.storage.impl.file.s3;
+
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+
+public class S3Record {
+
+  private static final String HEADER = "s3 bucket place holder";
+  @Getter
+  @Setter
+  private String bucketName;
+
+  @Getter
+  @Setter
+  private String entryName;
+
+  @Getter
+  @Setter
+  private String md5;
+
+  @Getter
+  @Setter
+  private LocalDateTime archivedDate;
+
+  public S3Record() {
+  }
+
+  public S3Record(String bucketName, String entryName, String contentMd5) {
+    this.archivedDate = LocalDateTime.now();
+    this.bucketName = bucketName;
+    this.entryName = entryName;
+    this.md5 = contentMd5;
+  }
+
+  public void write(String fileName) throws IOException {
+    FileOutputStream fileOutputStream = new FileOutputStream(fileName, false);
+    try (OutputStreamWriter writer = new OutputStreamWriter(fileOutputStream)) {
+      writer.write(HEADER +"\n");
+      writer.write(bucketName + "\n");
+      writer.write(entryName + "\n");
+      writer.write(md5 + "\n");
+      writer.write(archivedDate.toString() + "\n");
+    }
+  }
+
+  public void read(String fileName) throws IOException{
+    FileReader fileReader = new FileReader(fileName);
+    try (BufferedReader reader = new BufferedReader(fileReader)) {
+      if(!reader.readLine().equals(HEADER)){
+        throw new IOException("Invalid place holder");
+      }
+      bucketName = reader.readLine();
+      entryName = reader.readLine();
+      md5 = reader.readLine();
+      archivedDate = LocalDateTime.parse(reader.readLine());
+    }
+  }
+}
---
+++ src/main/java/io/mapsmessaging/storage/impl/file/s3/S3TransferApi.java
@@ -0,0 +1,63 @@
+/*
+ *   Copyright [2020 - 2022]   [Matthew Buckton]
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ */
+
+package io.mapsmessaging.storage.impl.file.s3;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectResult;
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+public class S3TransferApi {
+
+  private final AmazonS3 amazonS3;
+  private final String bucketName;
+
+  public S3TransferApi(AmazonS3 amazonS3, String bucketName){
+    this.amazonS3 = amazonS3;
+    this.bucketName = bucketName;
+  }
+
+  public boolean retrieve(String localFileName) throws IOException {
+    S3Record s3Record = new S3Record();
+    s3Record.read(localFileName);
+    File file = new File(localFileName);
+    if(!file.delete()){
+      System.err.println("Log this fact");
+    }
+    GetObjectRequest getObjectRequest = new GetObjectRequest(s3Record.getBucketName(), s3Record.getEntryName());
+    ObjectMetadata objectMetadata = amazonS3.getObject(getObjectRequest, file);
+    boolean success = s3Record.getMd5().equals(objectMetadata.getContentMD5());
+    if(!success) {
+      throw new IOException("MD5 signatures do not match");
+    }
+    amazonS3.deleteObject(s3Record.getBucketName(), s3Record.getEntryName());
+    return success;
+  }
+
+  public boolean archive(String path, String localFileName) throws IOException {
+    File file = new File(localFileName);
+    String entryName = path+"/"+UUID.randomUUID();
+    PutObjectResult putObjectResult = amazonS3.putObject(bucketName, entryName, file);
+    S3Record s3Record = new S3Record(bucketName, entryName,  putObjectResult.getContentMd5());
+    s3Record.write(localFileName);
+    return true;
+  }
+}
