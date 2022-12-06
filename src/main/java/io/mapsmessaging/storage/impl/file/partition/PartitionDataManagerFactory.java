336903385dc243565019613e316326fb68ca8dea
Matthew Buckton <matthew@buckton.org>
2022-12-06 13:03:42 +1100

    ensure we use the right constructor
    Signed-off-by: Matthew Buckton <matthew@buckton.org>

--- src/main/java/io/mapsmessaging/storage/impl/file/partition/PartitionDataManagerFactory.java
+++ src/main/java/io/mapsmessaging/storage/impl/file/partition/PartitionDataManagerFactory.java
@@ -26,9 +26,9 @@ import java.util.ServiceLoader;
 
 public class PartitionDataManagerFactory<T extends Storable> {
 
-  private static final PartitionDataManagerFactory<?> instance = new PartitionDataManagerFactory<>();
+  private static final PartitionDataManagerFactory instance = new PartitionDataManagerFactory<>();
 
-  public static PartitionDataManagerFactory getInstance(){
+  public static <T extends Storable> PartitionDataManagerFactory<T> getInstance(){
     return instance;
   }
 
